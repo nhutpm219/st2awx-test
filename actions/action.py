1b4a87ab458b3158136ca55d517cd542c89e75f1
Nhut Hitachi <nhut.phung@hitachivantara.com>
2021-06-29 08:03:19 +0000

    tenth

--- actions/action.py
+++ actions/action.py
@@ -1,8 +1,10 @@
 import sys
 import requests
-
+from st2common.runners.base_action import Action
 #templateid=sys.argv[1]
 from requests.auth import HTTPBasicAuth
 
-res = requests.post('http://20.198.234.27/api/v2/job_templates/'+'9'+'/launch/', verify=False, auth=HTTPBasicAuth('admin','MinhNhut@#68'))
-print (res.content)
+class runpyfile(Action):
+    def run(self):
+        res = requests.post('http://20.198.234.27/api/v2/job_templates/'+'9'+'/launch/', verify=False, auth=HTTPBasicAuth('admin','MinhNhut@#68'))
+        print (res.content)
