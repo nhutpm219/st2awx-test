ecd0895133214200e98cb355558c46368571aff5
Nhut Hitachi <nhut.phung@hitachivantara.com>
2021-06-29 08:54:11 +0000

    twelveth

--- actions/action.py
+++ actions/action.py
@@ -5,6 +5,7 @@ from st2common.runners.base_action import Action
 from requests.auth import HTTPBasicAuth
 
 class runpyfile(Action):
-    def run(self):
+    def run(self, payload):
         res = requests.post('http://20.198.234.27/api/v2/job_templates/'+'9'+'/launch/', verify=False, auth=HTTPBasicAuth('admin','MinhNhut@#68'))
         print (res.content)
+        return (payload)
